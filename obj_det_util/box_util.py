49aca5b9ccb66bf6591f58a4ea988b9dff563ece
이선훈 <seonhoon1002@gmail.com>
2020-12-30 12:07:10 +0900

    update

--- obj_det_util/box_util.py
+++ obj_det_util/box_util.py
@@ -1,21 +1,23 @@
 import numpy as np
 
-def xywh2xyxy(bbox):
-    bbox=bbox.astype(np.float64)
+def x1y1wh2xyxy(bbox):
+    bbox= np.array(bbox).astype(np.float32)
     bbox[2:4]=bbox[:2]+bbox[2:4]
-    return bbox.astype(np.int32)
+    return bbox.astype(np.int16)
 
-def xyxy2xywh(bbox):
-    bbox=bbox.astype(np.float64)
+def xyxy2x1y1wh(bbox):
+    bbox= np.array(bbox).astype(np.float32)
     bbox[2:4]=bbox[2:4]-bbox[:2]
-    return bbox.astype(np.int32)
+    return bbox.astype(np.int16)
 
 def cxcywh2xyxy(bbox):
+    bbox= np.array(bbox).astype(np.float32)
     bbox[:2]-=bbox[2:4]/2
     bbox[2:4]=bbox[:2]+bbox[2:4]
-    return bbox.astype(np.int32)
+    return bbox.astype(np.int16)
 
 def xyxy2cxcywh(bbox):
+    bbox= np.array(bbox).astype(np.float32)
     bbox[2:4]= bbox[2:4] - bbox[:2]
     bbox[:2]= bbox[:2] + bbox[2:4]/2
-    return bbox.astype(np.int32)
+    return bbox.astype(np.int16)
